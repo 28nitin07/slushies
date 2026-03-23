42e9bb922261d67ad6be2be0b3ca10576eeb67a9
Nitin S <28nitin07@gmail.com>
2026-03-23 23:28:14 +0530

    rename html files and add homepage + create route

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import random, datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    user_name = "Nitin"
-    return render_template('index.html', user_name=user_name)
+slushies = []
 
-@app.route('/testing')
-def testing():
-    user_name = "Testing"
-    return render_template('testing.html', user_name=user_name)
+flavors = ["Mango","Cola","Blue Raspberry","Strawberry","Grape","Lemon","Watermelon"]
+toppings = ["Boba","Jelly","Ice Cream","Whipped Crea","Sprinkles"]
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+color_map = {
+    "Mango": #fbbf24,
+    "Cola": #7c2d12
+    "Blue Raspberry": #3b82f6,
+    "Strawberry": #ef4444,
+    "Grape": #8b5cf6,
+    "Lemon": #eab308,
+    "Watermelon": #22c55e
+}
