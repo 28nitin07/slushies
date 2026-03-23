2791e167c77eee9f03261920c1ae025fd7cc838c
Nitin S <28nitin07@gmail.com>
2026-03-23 23:29:41 +0530

    add explore page and single slushie route

--- app.py
+++ app.py
@@ -17,3 +17,42 @@ color_map = {
     "Lemon": #eab308,
     "Watermelon": #22c55e
 }
+
+@app.route("/")
+def index():
+    return render_template("index.html", total=len(slushies))
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    if request.method == "POST":
+        flavor = request.form("flavor")
+        topping = request.form["topping"]
+
+        name = f"{flavor} {random.choice(['Blast','Storm','Rush'])}"
+
+        slushie = {
+            "id": len(slushies),
+            "name": name,
+            "flavor": flavor,
+            "topping": topping,
+            "color": color_map.get(flavor, '#ccc'),
+            "likes": 0,
+            "dislikes": 0
+        }
+
+        slushies.append(slushie)
+        return redirect(f"/slushie/{slushie['id']}")
+    
+    return render_template("create.html", flavors=flavors, toppings=toppings)
+
+@app.route("/explore")
+def explore():
+    return render_template("explore.html", slushies=slushies)
+
+@app.route("/slushie/<int:id>")
+def view_slushie(id):
+    slushie = slushies[id]
+    return render_template("slushie.html", slushie=slushie)
+
+
+
